38f2a4d6210177e06513271eb22cd87411163bef
xtayex <75450574@qq.com>
2020-03-02 20:08:52 +0800

    framework

--- Taxi.py
+++ Taxi.py
@@ -1,7 +1,15 @@
 class Taxi(object):
 	__schedule_list = []
-	def __init__(self, taxi_id, init_lon, init_lat):
+	def __init__(self, taxi_id, cur_lon, cur_lat, init_last_update_time, mobility_vector=None):
 		self.taxi_id = taxi_id
-		self.init_lon = init_lon
-		self.init_lat = init_lat
+		self.cur_lon = cur_lon
+		self.cur_lat = cur_lat
 		self.__schedule_list = []
+		self.__last_update_time = init_last_update_time
+		self.mobility_vector = mobility_vector
+	
+	def update_status(self, moment):
+		# 根据moment, __last_update_status和__schedule_list来更新taxi状态
+		# 状态包括, taxi当前位置(经纬度), mobility vector
+		# __last_update_time是用来更新状态的,
+		pass
--- main.py
+++ main.py
@@ -6,23 +6,31 @@ import datetime
 import pandas as pd
 from Node import Node
 from Taxi import Taxi
+import Queue
+from MobilityVector import MobilityVector
+import numpy as np
 
 conn = pymysql.connect(host='127.0.0.1', user='root',
                        passwd='', db='taxidb', port=3308, charset='utf8')
 cursor = conn.cursor(pymysql.cursors.SSCursor)
 
-taxi_mobility_vector = []
 request_mobility_vector = []
 mobility_cluster = []
 general_mobility_vector = []
 
-TYPICAL_SPEED = 13.8889 # 单位是m/s
+TYPICAL_SPEED = 13.8889  # 单位是m/s
 TAXI_TOTAL_NUM = 100
+EARLIEST_TIME = 0   # 预先从数据库中查询出最早时间, 避免每次都要查询一次. 肯定不会是0, 最后会修改
+TIME_OFFSET = 0
+SYSTEM_INIT_TIME = 0
+ORDER_COUNT = 0
 
 alpha = 0.999999921837146
 node_list = []
-taxi_list = [] # 里面包含有所有的taxi
-taxi_status_queue = [] # taxi的事件队列
+taxi_list = []  # 里面包含有所有的taxi
+taxi_status_queue = []  # taxi的事件队列
+mobility_cluster = []
+request_list = []
 
 
 def cosine_similarity(x, y):
@@ -49,62 +57,118 @@ def get_an_order(idx):
     return ret
 
 
-def determine(order):
-    flag = False
-    min_cos = 2
-    min_idx = 0
-    for idx, item in enumerate(general_mobility_vector):
-        vec1 = (lon1, lat1, lon2, lat2) = order[0:4]
-        cos_val = cosine_similarity(vec1, item)
-        if cos_val >= alpha and cos_val <= min_cos:
-            flag = True
-            min_cos = cos_val
-            min_idx = idx
-    if not flag:
-        # 新建一个mobility cluster
-        mobility_cluster.append([order])
-        general_mobility_vector.append(order)
-    else:
-        mobility_cluster[min_idx].append(order)
-        a = b = c = d = 0
-        leng = len(mobility_cluster[min_idx])
-        for it in range(leng):
-            a += mobility_cluster[min_idx][it][0]
-            b += mobility_cluster[min_idx][it][1]
-            c += mobility_cluster[min_idx][it][2]
-            d += mobility_cluster[min_idx][it][3]
-        general_mobility_vector[min_idx] = (a / leng, b / leng, c / leng, d / leng)    
-
-
 def system_init():
-    taxi_table = pd.read_csv('./data/taxi_info_list.csv') # 晚点添加这个文件, 里面是taxi的基本信息, 包括每辆taxi的编号和初始经纬度
+    ORDER_COUNT = 0
+    # 晚点添加这个文件, 里面是taxi的基本信息, 包括每辆taxi的编号和初始经纬度
+    taxi_table = pd.read_csv('./data/taxi_info_list.csv')
     df = pd.read_csv('./data/node_list_with_cluster.csv')
     for indexs in df.index:
         tmp = df.loc[indexs]
-        node_list.append(Node(tmp['real_id'], tmp['lon'], tmp['lat'], tmp['cluster_id']))
+        node_list.append(
+            Node(tmp['real_id'], tmp['lon'], tmp['lat'], tmp['cluster_id']))
     for taxi_it in taxi_table.index:
         tmp = df.loc[taxi_it]
-        taxi_list.append(Taxi(tmp['taxi_id'], tmp['init_lon'], tmp['init_lat']))
+        taxi_list.append(
+            Taxi(tmp['taxi_id'], tmp['init_lon'], tmp['init_lat'], SYSTEM_INIT_TIME - TIME_OFFSET))
 
+
+def request_fetcher(time_slot_start, time_slot_end):
+    sql = 'SELECT * FROM myorder WHERE start_time >= {} AND start_time <= {}'.format(
+        time_slot_start, time_slot_end)
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    return ret
+
+
+def update(request):
+    for taxi_it in taxi_list:
+        taxi_it.update_status(request['start_time'])
+    mobility_cluster.clear()
+    general_mobility_vector.clear()
+    for request_it in request_list:
+        vec1 = request_it[3:7]
+        min_cos = 2
+        min_idx = -1
+        for idx, gene_it in enumerate(general_mobility_vector):
+            cos_val = cosine_similarity(gene_it, vec1)
+            if cos_val < min_cos:
+                min_idx = idx
+                min_cos = cos_val
+        if min_idx != -1:
+            mobility_cluster[min_idx].append(MobilityVector(
+                vec1[0], vec1[1], vec1[2], vec1[3], 'REQ', request_it.request_id))
+            x = y = z = w = 0
+            for it in mobility_cluster[min_idx]:
+                x += it[0]
+                y += it[1]
+                z += it[2]
+                w += it[3]
+            leng = len(mobility_cluster[min_idx])
+            general_mobility_vector[min_idx] = MobilityVector(
+                x / leng, y / leng, z / leng, w / leng, 'REQ', request_it.request_id)
+        else:
+            mobility_cluster.append([MobilityVector(
+                vec1[0], vec1[1], vec1[2], vec1[3], 'REQ', request_it.request_id)])
+            general_mobility_vector.append(MobilityVector(
+                vec1[0], vec1[1], vec1[2], vec1[3], 'REQ', request_it.request_id))
+
+    for taxi_it in taxi_list:
+        vec2 = taxi_it.mobility_vector
+        min_cos = 2
+        min_idx = -1
+        for idx, gene_it in enumerate(general_mobility_vector):
+            cos_val = cosine_similarity(gene_it, vec2)
+            if cos_val < min_cos:
+                min_cos = cos_val
+                min_idx = idx
+        if min_idx != -1:
+            mobility_cluster[min_idx].append(MobilityVector(
+                vec2[0], vec2[1], vec2[2], vec2[3], 'TAXI', taxi_it.taxi_id))
+            x = y = z = w = 0
+            for it in mobility_cluster[min_idx]:
+                x += it[0]
+                y += it[1]
+                z += it[2]
+                w += it[3]
+            leng = len(mobility_cluster[min_idx])
+            general_mobility_vector[min_idx] = MobilityVector(
+                x / leng, y / leng, z / leng, w / leng, 'TAXI', taxi_it.taxi_id)
+        else:
+            mobility_cluster.append(
+                [MobilityVector(vec2[0], vec2[1], vec2[2], vec2[3], 'TAXI', taxi_it.taxi_id)])
+            general_mobility_vector.append(MobilityVector(
+                vec2[0], vec2[1], vec2[2], vec2[3], 'TAXI', taxi_it.taxi_id))
+
+
+def taxi_req_matching(req):
+    # taxi和request匹配
+    pass
+
+
+def taxi_routing(selected_taxi):
+    # 对匹配到的taxi进行路径规划
+    pass
 
 
 def main():
     system_init()
     order_index = 0
+    SYSTEM_INIT_TIME = time.time()
+    TIME_OFFSET = SYSTEM_INIT_TIME - EARLIEST_TIME
+    last_time = SYSTEM_INIT_TIME - TIME_OFFSET  # 初始化为开始时间
     while True:
-        release_time = time.time()
-        order_tuple = get_an_order(order_index)[0]
-        order_id = order_tuple[0]
-        order = order_tuple[3:8]
-        order_index += 1
-        determine(order)
-        deadline_time = release_time + datetime.timedelta(minutes=10)
-        req = Request(order_id, order[0], order[1], order[2], order[3], release_time, None, deadline_time)
-        waiting_time = deadline_time - release_time
-        search_range = waiting_time * TYPICAL_SPEED # 单位是m
-        
-        
-        time.sleep(30)
+        now_time = time.time() - TIME_OFFSET
+        last_time = now_time
+        reqs = request_fetcher(last_time, now_time)
+        if len(reqs) == 0:
+            continue
+        else:
+            for req_item in reqs:
+                request_list.append(req_item)
+                # 用当前moment来更新所有taxi, mobility_cluster和general_cluster
+                update(req_item)
+                selected_taxi = taxi_req_matching(req)
+
 
 main()
 

74e46b99ceb36b22434642921675ba38b7ef5e73
kingsleyljc <695801027@qq.com>
2020-03-12 21:12:17 +0800

    基本框架已经ok，不过bug可能一大堆

--- for_test.py
+++ for_test.py
@@ -1,26 +1,31 @@
-import osmnx as ox
-import pickle
-import networkx as nx
-import pandas as pd
+# import osmnx as ox
+# import pickle
+# import networkx as nx
+# import pandas as pd
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-df = pd.read_csv('./data/node_list_with_cluster.csv')
-tli = df.loc[:, 'real_id']
-map_file = open('./data/map.pickle', 'rb')
-print('1111')
-osm_map = pickle.load(map_file)
-path = nx.bidirectional_shortest_path(osm_map,source=tli[0],target=tli[10])
-G = nx.Graph()
+# df = pd.read_csv('./data/node_list_with_cluster.csv')
+# tli = df.loc[:, 'real_id']
+# map_file = open('./data/map.pickle', 'rb')
+# print('1111')
+# osm_map = pickle.load(map_file)
+# path = nx.bidirectional_shortest_path(osm_map,source=tli[0],target=tli[10])
+# G = nx.Graph()
 
 
-for idx,item in enumerate(path):
-    G.add_node(item)
-    if idx!=0:
-        G.add_edge(path[idx],path[idx-1])
-print(path)
+# for idx,item in enumerate(path):
+#     G.add_node(item)
+#     if idx!=0:
+#         G.add_edge(path[idx],path[idx-1])
+# print(path)
 # print('2222')
 # nx.draw(G,node_size = 1,edge_color='#555555')
 # nx.draw(osm_map,node_size = 0.7,edge_color='#ffffff')
 # print('asd')
 # plt.show()
+arr = [1,4,2,4,5]
+for idx,item in enumerate(arr):
+    if item == 4:
+        del arr[idx]
+print(arr)
